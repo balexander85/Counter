5255c4f0889a1285c08e0837ce7862c6dbe43aa9
Brian A <brian@dadgumsalsa.com>
2025-09-16 19:22:22 -0500

    Initial Commit

---
+++ Counter Watch App/CounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounterApp.swift
+//  Counter Watch App
+//
+//  Created by brian on 9/16/25.
+//
+
+import SwiftUI
+
+@main
+struct Counter_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Counter Watch AppTests/Counter_Watch_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Counter_Watch_AppTests.swift
+//  Counter Watch AppTests
+//
+//  Created by brian on 9/16/25.
+//
+
+import Testing
+@testable import Counter_Watch_App
+
+struct Counter_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
